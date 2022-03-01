c045deafa5c15b2bf68d95f068b1aec8debfa4cf
João <joaoroche.lima@gmail.com>
2022-03-01 13:30:19 -0300

    chore: remove unnecessary dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,10 +2,6 @@ module.exports = {
   "stories": [
     "../src/components/**/stories.tsx",
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+  "addons": ["@storybook/addon-essentials"],
   "framework": "@storybook/react"
 }
